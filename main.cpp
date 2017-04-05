b63c2a1475fa7184a3ea4bb2893112054eb9f8eb
denaedouglas <d102d638@ku.edu>
2017-04-05 14:50:42 -0500

    binary code in main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
-int main(){
+//the following code comes from cplusplus reference: http://www.cplusplus.com/reference/cstdio/fread/
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+int main () {
+  FILE * pFile;
+  long lSize;
+  char * buffer;
+  size_t result;
 
+  pFile = fopen ( "/home/denaedouglas/Documents/EECS_645/AddressTrace_LastIndex.bin" , "rb" );
+  if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
 
+  // obtain file size:
+  fseek (pFile , 0 , SEEK_END);
+  lSize = ftell (pFile);
+  rewind (pFile);
 
+	std::cout << "Size of file: " << lSize << "\n";
 
-  return(0);
+  // allocate memory to contain the whole file:
+  buffer = (char*) malloc (sizeof(char)*lSize);
+  if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
+
+  // copy the file into the buffer:
+  result = fread (buffer,4,lSize,pFile);
+
+	//this line will show how many addresses were read into memory
+	std::cout << "Number of Addresses: " << result << "\n";
+  /* the whole file is now loaded in the memory buffer. */
+
+  // terminate
+  fclose (pFile);
+  free (buffer);
+  return 0;
 }
+
