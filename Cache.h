c0da0ef04a9657a5f1d30f66bb96268159fbaa24
denaedouglas <d102d638@ku.edu>
2017-04-11 14:58:42 -0500

    Cache Definitions complete, readFile moved to Cache.cpp

--- Cache.h
+++ Cache.h
@@ -1,9 +1,28 @@
 #ifndef CACHE_H
 #define CACHE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
 
-#define CACHE_SIZE_EXP 15;
-#define CACHE_SIZE (1 << 15);
-#define ADDRESS_SIZE 32;
+#define CACHE_SIZE_EXP 15
+#define CACHE_SIZE (1 << 15)
+
+#define ADDRESS_SIZE 32
+
+#define CACHEASSOC_EXP 1
+#define CACHEASSOC (1 << CACHEASSOC_EXP)
+
+#define BLOCKSIZE_EXP 6
+#define BLOCKSIZE_NBR (1 << BLOCKSIZE_EXP)
+#define BLOCKSIZE_MASK (BLOCKSIZE_NBR - 1)
+
+#define LINES_EXP ((CACHE_SIZE_EXP) - (CACHEASSOC_EXP + BLOCKSIZE_EXP))
+#define LINES_NBR (1 << LINES_EXP)
+#define LINES_MASK (LINES_NBR - 1)
+
+#define TAG_EXP (ADDRESS_SIZE - BLOCKSIZE_EXP - LINES_EXP)
+#define TAG_NBR (1 << TAG_EXP)
+#define TAG_MASK (TAG_NBR - 1)
 
 struct Block{
   bool m_valid;
@@ -12,16 +31,14 @@ struct Block{
 
 class Cache{
 private:
-  //int cacheLines;
-  //int associativity;
   int m_hits;
   int m_misses;
-  void readBinFile();
+  void cacheSim();
   void replacement();
 
 
 public:
-  //Block m_cache[cacheLines][associativity];
+  Block m_cache[LINES_NBR][CACHEASSOC];
   Cache();
   ~Cache();
 };
