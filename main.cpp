9290f31a8c7893a83e7437f823ea7f88b81e6e78
lucianofasani <lucianofasani@gmail.com>
2017-04-11 20:18:10 -0500

    Removed stuff from main

--- main.cpp
+++ main.cpp
@@ -8,17 +8,6 @@ int main () {
 
   long bin, dec = 0, rem, num, base = 1;
 
-  std::cout << "Enter the binary number(1s and 0s) : ";
-  std::cin >> num;
-  bin = num;
-  while (num > 0)
-  {
-      rem = num % 10;
-      dec = dec + rem * base;
-      base = base * 2;
-      num = num / 10;
-  }
-  std::cout << "The decimal equivalent of " << bin << " : " << dec << "\n";
 
   return 0;
 
